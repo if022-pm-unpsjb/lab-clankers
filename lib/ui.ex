d435bf45a6aea15cd2fb59a6f547a5d046fe5b46
lucassanmartin <lucassanmartin76@gmail.com>
2025-10-16 19:00:20 -0300

    despues de la super clase de fran jueves 16/10 cumple rami 22222

--- lib/ui.ex
+++ lib/ui.ex
@@ -2,10 +2,19 @@ defmodule Libremarket.Ui do
 
   def node_compras(), do: :"compras@equipoalan"
 
+
+
   def comprar(id_producto, medio_de_pago, forma_de_entrega) do
     confirma_compra = Libremarket.Compras.Server.confirmarCompra()
-    if(confirma_compra) do
-      Libremarket.Compras.comprar({id_producto, medio_de_pago, forma_de_entrega}) # Libremarket.Compras.Server.comprar({id_producto, medio_de_pago, forma_de_entrega})
+
+    if confirma_compra do
+      # Generamos el id_compra acá
+      id_compra = :erlang.unique_integer([:positive])
+
+      # Llamamos directamente al flujo lógico, sin GenServer
+      Libremarket.Compras.comprar(id_compra, id_producto, medio_de_pago, forma_de_entrega)
+    else
+      {:error, :compra_no_confirmada}
     end
   end
 
